89604b143544c649e9cd8913ea2c7fed5479d830
Javafullstackvenu72 <venu996366@gmail.com>
2025-05-31 14:13:58 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,40 @@
+#!/bin/bash
+
+# Create a temporary directory for execution
+WORKDIR=$(mktemp -d)
+cp "$1" "$WORKDIR/"
+cd "$WORKDIR" || exit 1
+
+case "$1" in
+    *.java)
+        javac "$(basename "$1")" && java -cp . "$(basename "$1" .java)"
+        ;;
+    *.py)
+        python3 "$(basename "$1")"
+        ;;
+    *.c)
+        gcc "$(basename "$1")" -o main && ./main
+        ;;
+    *.cpp)
+        g++ "$(basename "$1")" -o main && ./main
+        ;;
+    *.js)
+        node "$(basename "$1")"
+        ;;
+    *.html|*.css)
+        cp -- * /usr/share/nginx/html/
+        nginx -g "daemon off;"
+        ;;
+    *)
+        echo "Unsupported file type"
+        exit 1
+        ;;
+esac
+
+# Cleanup temp directory
+rm -rf "$WORKDIR"
+
+
 
 # #!/bin/bash
 
@@ -34,43 +71,6 @@
 
 # # Cleanup
 # rm -rf "$WORKDIR"
-
-
-#!/bin/bash
-
-# Create a temp directory for this run
-WORKDIR=$(mktemp -d)
-cp "$1" "$WORKDIR/"
-cd "$WORKDIR" || exit 1
-
-case "$1" in
-    *.java)
-        javac "$(basename "$1")" && java -cp . "$(basename "$1" .java)"
-        ;;
-    *.py)
-        python3 "$(basename "$1")"
-        ;;
-    *.c)
-        gcc "$(basename "$1")" -o main && ./main
-        ;;
-    *.cpp)
-        g++ "$(basename "$1")" -o main && ./main
-        ;;
-    *.js)
-        node "$(basename "$1")"
-        ;;
-    *.html|*.css)
-        cp -- * /usr/share/nginx/html/
-        nginx -g "daemon off;"
-        ;;
-    *)
-        echo "Unsupported file type"
-        exit 1
-        ;;
-esac
-
-# Cleanup
-rm -rf "$WORKDIR"
 
 
 # #!/bin/bash
